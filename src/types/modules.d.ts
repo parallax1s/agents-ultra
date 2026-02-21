81e7f4097440712a0544808b50957ecbe77cbf6c
parallax1s <peilosunited@gmail.com>
2026-02-21 19:19:56 +0100

    Fix React shim useRef overload to support non-null refs

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -4,6 +4,7 @@ declare module 'react' {
   export function useCallback<T extends (...a: any[]) => any>(cb: T, deps: readonly any[]): T;
   export function useEffect(cb: () => void | (() => void), deps?: readonly any[]): void;
   export function useMemo<T>(fn: () => T, deps: readonly any[]): T;
+  export function useRef<T>(init: T): { current: T };
   export function useRef<T>(init: T | null): { current: T | null };
   export function useState<S>(init: S): [S, (s: S) => void];
   export const Fragment: any;
